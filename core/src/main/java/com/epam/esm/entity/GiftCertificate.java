7cb358c7620a51e37e32848fec21b804768faca8
Dmitriy Karpov <dmitriy_karpov@epam.com>
2022-04-12 21:27:26 +0300

    created entity Tag,GiftCertificate

---
+++ core/src/main/java/com/epam/esm/entity/GiftCertificate.java
@@ -0,0 +1,33 @@
+package com.epam.esm.entity;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.ZonedDateTime;
+import java.util.Objects;
+@Data
+@Builder
+public class GiftCertificate  {
+
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private int duration;
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSX")
+    private ZonedDateTime createDate;
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSX")
+    private ZonedDateTime lastUpdateDate;
+
+    @JsonCreator
+    public GiftCertificate() {
+    }
+}
