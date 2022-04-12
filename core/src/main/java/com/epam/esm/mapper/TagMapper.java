04f1c34e4226bea3348764fedaf8723aaa561258
Dmitriy Karpov <dmitriy_karpov@epam.com>
2022-04-12 21:28:36 +0300

    created mapper for entity .

---
+++ core/src/main/java/com/epam/esm/mapper/TagMapper.java
@@ -0,0 +1,19 @@
+package com.epam.esm.mapper;
+
+import com.epam.esm.entity.Tag;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static com.epam.esm.constant.ColumnName.*;
+
+public class TagMapper implements RowMapper<Tag> {
+    @Override
+    public Tag mapRow(ResultSet rs, int rowNum) throws SQLException {
+        return Tag.builder()
+                .id(rs.getLong(TAG_ID))
+                .name(rs.getString(TAG_NAME))
+                .build();
+    }
+}
