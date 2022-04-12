4f462f8799f32c804f3e76c4b0796c6a679925ff
Dmitriy Karpov <dmitriy_karpov@epam.com>
2022-04-12 21:31:08 +0300

    added column name constant.

---
+++ core/src/main/java/com/epam/esm/constant/ColumnName.java
@@ -0,0 +1,13 @@
+package com.epam.esm.constant;
+
+public final class ColumnName {
+    public static final String TAG_ID = "id";
+    public static final String TAG_NAME = "name";
+    public static final String GIFT_CERTIFICATE_ID = "id";
+    public static final String GIFT_CERTIFICATE_NAME = "name";
+    public static final String GIFT_CERTIFICATE_PRICE = "price";
+    public static final String GIFT_CERTIFICATE_DURATION = "duration";
+    public static final String GIFT_CERTIFICATE_DESCRIPTION = "description";
+    public static final String GIFT_CERTIFICATE_CREATE_TIME = "create_date";
+    public static final String GIFT_CERTIFICATE_LAST_UPDATE_DATE = "last_update_date";
+}
