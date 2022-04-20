0868e5c370ed80f03ceeadd27e952bea8637ee46
Dmitriy Karpov <dmitriy_karpov@epam.com>
2022-04-20 20:27:02 +0300

    added exception constants

---
+++ service/src/main/java/com/epam/esm/constant/ExceptionMessageConstant.java
@@ -0,0 +1,17 @@
+package com.epam.esm.constant;
+
+public final class ExceptionMessageConstant {
+    public static final String TAG_EXISTS = "message.tagExists";
+    public static final String NOT_FOUND_MSG = "message.notFound";
+    public static final String TAG_INVALID_MSG = "message.tagInvalid";
+    public static final String TAG_DOES_NOT_EXISTS = "message.tagDoesntExist";
+    public static final String CANT_FIND_TAG_BY_ID_MSG = "message.cantFindTagById";
+    public static final String CANT_FIND_TAG_BY_NAME_MSG = "message.cantFindTagByName";
+    public static final String CANT_UPDATE_INVALID_NAME_MSG = "message.cantUpdateInvalidName";
+    public static final String GIFT_CERTIFICATE_EXISTS_MSG   = "message.giftCertificateExists";
+    public static final String GIFT_CERTIFICATE_INVALID_MSG = "message.giftCertificateInvalid";
+    public static final String CANT_UPDATE_INVALID_PRICE_MSG = "message.cantUpdateInvalidPrice";
+    public static final String CANT_UPDATE_GIFT_CERTIFICATE = "message.cantUpdateGiftCertificateById";
+    public static final String CANT_UPDATE_INVALID_DURATION_MSG = "message.cantUpdateInvalidDuration";
+    public static final String CANT_UPDATE_INVALID_DESCRIPTION_MSG = "message.cantUpdateInvalidDescription";
+}
